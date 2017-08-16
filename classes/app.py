c88c775ea28212108d00313e606199b85967798e
Patrick Luwum <luwyxx@gmail.com>
2017-08-16 08:49:33 +0300

    Added methods to provide create item functionality

--- classes/app.py
+++ classes/app.py
@@ -1,9 +1,17 @@
 from classes.user import User
+from classes.list_item import ListItem
 class App():
     def __init__(self):
         self.users = {}
+        self.listItems = {}
+        self.shoppingLists = {}
 
     def registerUser(self, first_name, last_name, email, password):
         user = User(first_name, last_name, email, password)
         self.users[email] = {'first_name':first_name, 'last_name':last_name, 'email':email, 'password':password}
+        return True
+
+    def registerItem(self, name, description):
+        item = ListItem(name, description)
+        self.listItems[name] = {'name':name, 'description':description}
         return True
