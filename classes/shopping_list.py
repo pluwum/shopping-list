ae11bfc2c22c8b0dc6c0c52b4ff4dfda41ee1920
Patrick Luwum <luwyxx@gmail.com>
2017-08-16 09:36:13 +0300

    Added implementation for Shopping list class

---
+++ classes/shopping_list.py
@@ -0,0 +1,10 @@
+"""This file contains the Shopping List class definition
+This class represents each shopping list and the generic
+attributes it has
+"""
+
+
+class ShoppingList():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
