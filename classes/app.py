13b38d821ada3f6f038a3fbab1f5734393ad0e0a
Patrick Luwum <luwyxx@gmail.com>
2017-08-16 09:35:11 +0300

    Added implementation for create list feature

--- classes/app.py
+++ classes/app.py
@@ -1,5 +1,20 @@
+"""App Class
+This file contains the App class c which is the main application class
+The class handles all actions of a user on the application by leveraging 
+other classes
+the are 
+    - Registration/signup
+    - Login
+    - Share list
+    - Create list
+    - Delete list
+    - View list
+"""
 from classes.user import User
 from classes.list_item import ListItem
+from classes.shopping_list import ShoppingList
+
+
 class App():
     def __init__(self):
         self.users = {}
@@ -8,10 +23,16 @@ class App():
 
     def registerUser(self, first_name, last_name, email, password):
         user = User(first_name, last_name, email, password)
-        self.users[email] = {'first_name':first_name, 'last_name':last_name, 'email':email, 'password':password}
+        self.users[email] = {'first_name': first_name,
+                             'last_name': last_name, 'email': email, 'password': password}
         return True
 
     def registerItem(self, name, description):
         item = ListItem(name, description)
-        self.listItems[name] = {'name':name, 'description':description}
+        self.listItems[name] = {'name': name, 'description': description}
+        return True
+
+    def createShoppingList(self, name, description):
+        shoppingList = ShoppingList(name, description)
+        self.shoppingLists[name] = {'name': name, 'description': description}
         return True
