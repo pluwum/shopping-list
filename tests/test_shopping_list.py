8b53a8dad2fc1af720a1ac549ddf77852c1999b9
Patrick Luwum <luwyxx@gmail.com>
2017-08-16 08:47:06 +0300

    Added testcases for testing Create item functionality

--- tests/test_shopping_list.py
+++ tests/test_shopping_list.py
@@ -6,7 +6,6 @@ shopping list app
 from unittest import TestCase
 from classes.app import App
 
-
 class TestRegisterUser(TestCase):
     
     """Handles Test cases for user sign up features"""
@@ -18,7 +17,22 @@ class TestRegisterUser(TestCase):
         usersBefore = len(self.app.users)
         result = self.app.registerUser('patrick','alvin','luwyxx@gmail.com','psw12345!')
         usersAfter = len(self.app.users)
-        
+
         """check that user object creates succesfully 
         and number of users increased by users created"""
         self.assertListEqual([True, usersAfter], [result, usersBefore+1])
+
+class TestCreateItem(TestCase):
+    """ Handles test cases for create new item"""
+
+    def setUp(self):
+        self.app = App()
+
+    def test_creates_item_successfully(self):
+        itemsBefore = len(self.app.listItems)
+        result = self.app.registerItem('sugar','sweet substance that kills people')
+        itemsAfter = len(self.app.listItems)
+
+        """check that user object creates succesfully 
+        and number of items increased by items created"""
+        self.assertListEqual([True, itemsAfter], [result, itemsBefore+1])
