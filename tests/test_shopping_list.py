b316ace6f913155290ab6ee452268044be45ab7a
Patrick Luwum <luwyxx@gmail.com>
2017-08-16 09:34:41 +0300

    Added tests for create list feature

--- tests/test_shopping_list.py
+++ tests/test_shopping_list.py
@@ -6,8 +6,9 @@ shopping list app
 from unittest import TestCase
 from classes.app import App
 
+
 class TestRegisterUser(TestCase):
-    
+
     """Handles Test cases for user sign up features"""
 
     def setUp(self):
@@ -15,12 +16,14 @@ class TestRegisterUser(TestCase):
 
     def test_register_successfully(self):
         usersBefore = len(self.app.users)
-        result = self.app.registerUser('patrick','alvin','luwyxx@gmail.com','psw12345!')
+        result = self.app.registerUser(
+            'patrick', 'alvin', 'luwyxx@gmail.com', 'psw12345!')
         usersAfter = len(self.app.users)
 
         """check that user object creates succesfully 
         and number of users increased by users created"""
-        self.assertListEqual([True, usersAfter], [result, usersBefore+1])
+        self.assertListEqual([True, usersAfter], [result, usersBefore + 1])
+
 
 class TestCreateItem(TestCase):
     """ Handles test cases for create new item"""
@@ -30,9 +33,27 @@ class TestCreateItem(TestCase):
 
     def test_creates_item_successfully(self):
         itemsBefore = len(self.app.listItems)
-        result = self.app.registerItem('sugar','sweet substance that kills people')
+        result = self.app.registerItem(
+            'sugar', 'sweet substance that kills people')
         itemsAfter = len(self.app.listItems)
 
         """check that user object creates succesfully 
         and number of items increased by items created"""
-        self.assertListEqual([True, itemsAfter], [result, itemsBefore+1])
+        self.assertListEqual([True, itemsAfter], [result, itemsBefore + 1])
+
+
+class TestCreateList(TestCase):
+    """ Handles test cases for create list feature"""
+
+    def setUp(self):
+        self.app = App()
+
+    def test_creates_list_successfully(self):
+        listsBefore = len(self.app.shoppingLists)
+        result = self.app.createShoppingList(
+            'Back to school', 'school shopping list')
+        listsAfter = len(self.app.shoppingLists)
+
+        """check that user object creates succesfully 
+        and number of items increased by items created"""
+        self.assertListEqual([True, listsAfter], [result, listsBefore + 1])
